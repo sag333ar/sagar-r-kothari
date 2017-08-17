552af72a359fcb0438489e1e5e12202c24db52dc
Sagar R. Kothari <sag333ar@gmail.com>
2017-08-17 19:22:44 +0530

    Restore data

--- nToDo/nToDo/nToDo/ViewController.swift
+++ nToDo/nToDo/nToDo/ViewController.swift
@@ -18,22 +18,12 @@ class ViewController: UIViewController {
   @IBOutlet weak var tableView: UITableView!
   override func viewDidLoad() {
     super.viewDidLoad()
+//    storeData()
     loadData()
-  }
-  
-  func getTomorrowDate() -> Date {
-    let cal = NSCalendar.current
-    let now = Date()
-    let compSet = Set(arrayLiteral: Calendar.Component.day, Calendar.Component.month, Calendar.Component.year)
-    var comp = cal.dateComponents(compSet as Set<Calendar.Component>, from: now)
-    comp.timeZone = TimeZone.current
-    comp.day = comp.day! + 1
-    return cal.date(from: comp)!
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
   
   func loadData() {
@@ -57,7 +47,7 @@ class ViewController: UIViewController {
     } catch {
       tasks = []
     }
-    // tasks
+    tableView.reloadData()
   }
 
 }
@@ -86,6 +76,66 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     tableView.deselectRow(at: indexPath, animated: true)
   }
 
+}
+
+extension ViewController {
+
+  func getTomorrowDate() -> Date {
+    let cal = NSCalendar.current
+    let now = Date()
+    let compSet = Set(arrayLiteral: Calendar.Component.day, Calendar.Component.month, Calendar.Component.year)
+    var comp = cal.dateComponents(compSet as Set<Calendar.Component>, from: now)
+    comp.timeZone = TimeZone.current
+    comp.day = comp.day! + 1
+    return cal.date(from: comp)!
+  }
+
+  func storeData() {
+    let location1 = Location(context: Context)
+    location1.title = "Brahma Suncity"
+    let location2 = Location(context: Context)
+    location2.title = "Viman Nagar"
+    let location3 = Location(context: Context)
+    location3.title = "Pimpri Chinchwad"
+    let location4 = Location(context: Context)
+    location4.title = "Lunkad Gardens"
+    let location5 = Location(context: Context)
+    location5.title = "Durshet, Maharashtra"
+    let location6 = Location(context: Context)
+    location6.title = "Swati Greens"
+    let location7 = Location(context: Context)
+    location7.title = "Shukan Homes"
+    
+    let tag1 = Tag(context: Context)
+    tag1.title = "Important"
+    let tag2 = Tag(context: Context)
+    tag2.title = "Dreams"
+    let tag3 = Tag(context: Context)
+    tag3.title = "Ideas"
+    let tag4 = Tag(context: Context)
+    tag4.title = "Budget"
+    let tag5 = Tag(context: Context)
+    tag5.title = "Expenses"
+    
+    let list1 = List(context: Context)
+    list1.title = "Inbox"
+    
+    AppDel.saveContext()
+    loadData()
+    
+    let task1 = Task(context: Context)
+    task1.title = "Remember the milk"
+    task1.priority = 1
+    task1.estimate = 1
+    task1.start = Date()
+    task1.due = getTomorrowDate()
+    task1.locations = locations[0]
+    task1.list = lists[0]
+    task1.tags = NSSet(arrayLiteral: tags[0], tags[1])
+    task1.completed = false
+    AppDel.saveContext()
+    loadData()
+  }
 }
 
 /*
